f465e8bf359e8f0703d40699789adf62f83e8ffe
rgbutler-lab <rgbutler@aggies.ncat.edu>
2025-09-25 17:02:13 -0400

    Update rgbutler_assignment_4.py

--- rgbutler_assignment_4.py
+++ rgbutler_assignment_4.py
@@ -12,3 +12,21 @@ print(f"Study Hours: {study_hours}")
 print(f"Social Points: {social_points}")
 print(f"Stress Level: {stress_level}")
 
+print("Choose your course load:")
+print("A) Light (12 credits)")
+print("B) Medium (15 credits)")
+print("C) Heavy (18 credits)")
+
+choice = input("Your choice: ")
+A = ("Light Difficulty: (12 Credits)")
+B = ("Medium Difficulty: (15 Credits)")
+C = ("Hard Difficulty: (18 Credits)")
+
+if choice == "A":
+    print(f"Mode chosen: {A}")
+elif choice == "B":
+    print(f"Mode chosen: {B}")
+elif choice == "C":
+    print(f"Mode chosen: {C}")
+else:
+    print("Invalid Choice: Choose again.")
