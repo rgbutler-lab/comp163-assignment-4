b326f6e046db756e9fb2c4f1c8f69226f64af3db
rgbutler-lab <rgbutler@aggies.ncat.edu>
2025-09-25 16:18:35 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ rgbutler_assignment_4.py
@@ -0,0 +1,14 @@
+student_name = "Ryleigh"
+current_gpa = 3.8 #Float between 1.0 and 4.0
+study_hours = 40 #Integer (ex. 20)
+social_points = 50 #Integer (ex. 40)
+stress_level = 65 #Integer 1-100
+
+#Display Welcome stats
+print(f"Welcome {student_name} to your Academic Profile")
+print("")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
+
